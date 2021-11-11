da76830c64490a982c3b1f8d30be49dd0b60114d
Cristian Rojas <injcristianrojas@gmail.com>
2021-11-11 16:37:59 -0300

    Traverses subdirectories now. Implements #2

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
 $targetdir = './files/';
 
+function getDirContents($path) {
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+    $files = array(); 
+    foreach ($rii as $file) {
+        if (!$file->isDir()) {
+            $files[] = $file->getPathname();
+        }
+    }
+    return $files;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $uploadfile = $targetdir . $_FILES['file']['name'];
     move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
 } else {
-    $files = array();
-    if (is_dir($targetdir)) {
-        if ($dh = opendir($targetdir)) {
-            while (($file = readdir($dh)) !== false) {
-                if (($file != '.') && ($file != '..')) {
-                    $files[] = $file;
-                }
-            }
-            closedir($dh);
-        }
-    }
+    $files = getDirContents($targetdir);
     sort($files);
 
+    echo '<p>File list</p>';
+
     foreach ($files as $file) {
-        echo '<div><a href="'.$targetdir.$file.'">'.$file.'</a></div>';
+        echo '<div><a href="'.$file.'">'.str_replace($targetdir, '', $file).'</a></div>';
     }
 }
